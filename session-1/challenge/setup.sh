304a06085c0b1452239277f4fbce9ead433242d2
BaeSenseii <xxbaemaxx@gmail.com>
2025-02-05 01:42:13 +0800

    Update setup.sh

--- session-1/challenge/setup.sh
+++ session-1/challenge/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker stop d1_challenge
-docker container rm -f d1_challenge
-docker rmi -f d1_challenge
-docker build -t d1_challenge .
-docker run -p 8881:8881 -it -d --name d1_challenge d1_challenge
+docker stop session1_challenge
+docker container rm -f session1_challenge
+docker rmi -f session1_challenge
+docker build -t session1_challenge .
+docker run -p 8881:8881 -it -d --name session1_challenge session1_challenge
